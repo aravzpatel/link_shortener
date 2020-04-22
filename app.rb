f406a0f99498a318afe1cce873e2a0c07c2f8a07
Arav Patel <arav.patel@gmail.com>
2020-04-22 17:03:07 +0100

    Working redirect

--- app.rb
+++ app.rb
@@ -1,13 +1,38 @@
 require 'sinatra'
+require 'shotgun'
+require 'base64'
+require 'pstore'
 
 get '/:url' do
-    "URL is #{params[:url]}"
+    redirect "http://" + ShortURL.read(params[:url])
 end
 
 get '/' do
-    "Enter your URL"
+    "Enter your URL using cURL POST"
 end
 
 post '/' do
-    "New URL added: #{params[:url]}\n"
+    url = generate_short_url(params[:url])
+    "Your shortened URL is: #{url} \n"
+end
+
+def generate_short_url(original)
+    ShortURL.save(Base64.encode64(original)[0..6], original)
+
+    "localhost:4567/" + Base64.encode64(original)[0..6]
+end
+
+class ShortURL
+    def self.save(encoded, original)
+        store.transaction { |t| store[encoded] = original}
+    end
+
+    def self.read(encoded)
+        store.transaction { store[encoded] }
+    end
+
+    def self.store
+        @store ||= PStore.new("shortened_urls.db")
+    end
+        
 end
